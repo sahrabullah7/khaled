50e6f66c400f3d733a6dc6e503c0367bc8bb9e3c
sahrabullah7 <145543132+sahrabullah7@users.noreply.github.com>
2024-04-30 00:54:45 +0200

    Update test.py
    first test

--- test.py
+++ test.py
@@ -3,3 +3,12 @@ import unittest
 
 # Import the functions to be tested from the code
 from codes import char_to_baudot, baudot_to_char, encrypt_phrase, decrypt_phrase
+
+# Define a test class for the unittest
+class TestBaudotEncryption(unittest.TestCase):
+    # test the first function
+    def test_char_to_baudot(self):
+        # Test individual character to Baudot code conversion
+        self.assertEqual(char_to_baudot('A'), '00011')
+        self.assertEqual(char_to_baudot(' '), '00000')
+        self.assertEqual(char_to_baudot('1'), '')  # Invalid characters
