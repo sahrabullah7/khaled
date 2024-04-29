8cd2343b51af5445200d43d5322f02dfacd9defb
sahrabullah7 <145543132+sahrabullah7@users.noreply.github.com>
2024-04-30 00:53:40 +0200

    Update test.py

---
+++ test.py
@@ -0,0 +1,5 @@
+# Import the 'unittest' module
+import unittest
+
+# Import the functions to be tested from the code
+from codes import char_to_baudot, baudot_to_char, encrypt_phrase, decrypt_phrase
